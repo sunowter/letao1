679d7278be73d82daede7a2fcb0d0a156bf76441
STT <1475814986@qq.com>
2018-08-22 19:16:03 +0800

    ‘搜索中心页面布局完成

---
+++ public/front/js/search.js
@@ -0,0 +1,10 @@
+//点击回退按钮回到上一页
+$('#back').click(function(){
+history.back()
+
+
+
+
+
+
+})
--- public/front2/js/common.js
+++ public/front2/js/common.js
@@ -16,3 +16,29 @@ var gallery = mui('.mui-slider');
 gallery.slider({
   interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
 });
+
+
+// 作用: 专门用于解析地址栏参数
+function getSearch( k ) {
+  // 获取地址栏参数
+  var search = location.search;   // "?name=pp&age=18&desc=%E5%B8%85"
+
+  // 将其解码的中文
+  search = decodeURI( search );   // "?name=pp&age=18&desc=帅"
+
+  // 去掉问号
+  search = search.slice( 1 );    // "name=pp&age=18&desc=帅"
+
+  // 通过 & 分割成数组
+  var arr = search.split( "&" );  //  ["name=pp", "age=18", "desc=帅"]
+
+  var obj = {};
+  arr.forEach(function( v, i ) {  // v 表示每项 "name=pp"
+    var key = v.split("=")[0];  // name
+    var value = v.split("=")[1];  // pp
+    // [] 语法会解析变量
+    obj[ key ] = value;
+  })
+
+  return obj[ k ];
+}
